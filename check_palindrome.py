72bbd51e8781677c3176d1bdfe338f143f210942
Akhilesh Kumar Singh <akhilesh7218@gmail.com>
2025-08-20 13:25:00 +0530

    add check_palindrome.py'

---
+++ check_palindrome.py
@@ -0,0 +1,29 @@
+# 1st way without function
+
+num = int(input('Enter any number to check palindrome: '))
+n = num
+result = 0
+while n>0:
+    last_digit = n%10
+    result = result*10 + last_digit
+    n = n//10
+if result == num:
+    print('Palindrome')
+else:
+    print('Not a Palindrome')
+
+
+
+
+# 2nd way using function
+
+def check_palindrome(num):
+    n = num
+    result = 0
+    while n>0:
+        last_digit = n%10
+        result = result*10 + last_digit
+        n = n//10
+    return result == num
+number = int(input('Enter any number to check palindrome: '))
+print(check_palindrome(number))
